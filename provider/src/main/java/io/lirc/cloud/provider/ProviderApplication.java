9c0df27c4b5834754cb02bb608170e05a28d3606
lircstar <lrcstar@hotmail.com>
2018-06-10 07:10:31 +0800

    rm control in server

--- provider/src/main/java/io/lirc/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/io/lirc/cloud/provider/ProviderApplication.java
@@ -2,13 +2,20 @@ package io.lirc.cloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ProviderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
+		Scanner scan = new Scanner(System.in);
+		String port = scan.nextLine();
+		//SpringApplication.run(ProviderApplication.class, args);
+		new SpringApplicationBuilder(ProviderApplication.class).properties(
+				"server.port=" + port).run(args);
 	}
 }
--- server/src/main/java/io/lirc/cloud/server/ServerApplication.java
+++ server/src/main/java/io/lirc/cloud/server/ServerApplication.java
@@ -2,13 +2,20 @@ package io.lirc.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+
+		Scanner scan = new Scanner(System.in);
+		String profiles = scan.nextLine();
+		//SpringApplication.run(ServerApplication.class, args);
+		new SpringApplicationBuilder(ServerApplication.class).profiles(profiles).run(args);
 	}
 }
